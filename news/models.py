f933a49e23fff4d68cc60af96d87312bbdf35cc8
Marcelo Tokarnia <marcelo.tokarnia@gmail.com>
2018-03-04 03:42:06 -0300

    restrict news categories

--- news/models.py
+++ news/models.py
@@ -12,5 +12,9 @@ class News(BaseModel):
     title = models.CharField(max_length=1024)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    category = models.CharField(max_length=128)
+    category = models.CharField(max_length=128, choices=(("POLITICS", ) * 2,
+                                                         ("TECH", ) * 2,
+                                                         ("SCIENCE", ) * 2,
+                                                         ("SPORTS", ) * 2,
+                                                         ("BUSINESS", ) * 2,))
     thumbnail = models.OneToOneField(Picture, on_delete=models.CASCADE, null=True, blank=True)
